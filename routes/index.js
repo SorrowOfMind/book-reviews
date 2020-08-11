14591809aa3de1021b7244f7d4e6eb8939cbc6f0
Basia <b.kumor@yahoo.com>
2020-08-11 18:27:35 +0200

    dashboard + review model

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,22 @@
 const router = require('express').Router();
 const {ensureAuth, ensureGuest} = require('../middlware/auth');
+const Review = require('../models/Review');
 
 router.get('/', ensureGuest, (req, res) => {
     res.render('login', {layout: 'login'});
 });
 
-router.get('/dashboard', ensureAuth, (req, res) => {
-    res.render('dashboard');
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const reviews = Review.find({user: req.user.id}).lean();
+        res.render('dashboard', {
+            name: req.user.firstName,
+            reviews
+        });
+    } catch (err) {
+        console.error(err);
+    }
+    
 })
 
 module.exports = router;
