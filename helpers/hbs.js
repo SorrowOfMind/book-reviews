b8c8d9d2d53336cf78979a03ba6c34bd848aca1e
Basia <b.kumor@yahoo.com>
2020-08-16 12:11:27 +0200

    show single review for editing

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -17,9 +17,9 @@ module.exports = {
     editIcon: function (reviewUser, loggedUser, reviewId, floating=true) {
         if (reviewUser._id.toString() === loggedUser._id.toString()) {
             if (floating) {
-                return `<a href="/stories/edit/${reviewId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fa-small"></i></a>`
+                return `<a href="/reviews/edit/${reviewId}" class="btn-floating halfway-fab blue"><i class="fas fa-edit fa-small"></i></a>`
             } else {
-                return `<a href="/stories/edit/${reviewId}"><i class="fas fa-edit"></i></a>`
+                return `<a href="/reviews/edit/${reviewId}"><i class="fas fa-edit"></i></a>`
             }
         } else return '';
     }
